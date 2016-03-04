02c2d25eda1807ea307531f3a86e14848a9c28c0
Jeremy Mauzy <jeremymauzy@gmail.com>
2016-03-03 23:43:12 -0600

    Add exception handling for non-existant link

--- app/models/redirector.rb
+++ app/models/redirector.rb
@@ -1,7 +1,11 @@
 class Redirector
 
   def initialize(url_hash)
-    @link = get_link(url_hash)
+    begin
+      @link = get_link(url_hash)
+    rescue ActiveRecord::RecordNotFound => e
+      raise e
+    end
   end
 
   def target_url
@@ -11,9 +15,7 @@ class Redirector
   private
 
   def get_link(url_hash)
-    Link.find_by(url_hash: url_hash)
+      Link.find_by!(url_hash: url_hash)
   end
   
 end
-
-
--- spec/models/redirector_spec.rb
+++ spec/models/redirector_spec.rb
@@ -1,13 +1,29 @@
 require "rails_helper"
 require "support/link_helper"
 
+describe "Redirector" do
+  it "finds a record with a hash matching the first parameter" do
+
+    expect(Link).to receive(:find_by!).with(url_hash: "test")
+
+    Redirector.new("test")
+  end
+
+  it "raises an exception if record is not found" do
+
+    expect {Redirector.new("missing link")}.to raise_error(ActiveRecord::RecordNotFound)
+  end
+end
+
+
 describe "#target_url" do
   include LinkHelper
 
   it "finds a link by url_hash and returns the url" do
+
     link = generate_link
     url_hash = link.url_hash
-    allow(Link).to receive(:find_by).with(url_hash: url_hash).and_return(link)
+    allow(Link).to receive(:find_by!).with(url_hash: url_hash).and_return(link)
 
     redirector = Redirector.new(url_hash)
 
