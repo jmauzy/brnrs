1764ea5690b76d71ffae2277fac1068b2b7ad726
Jeremy Mauzy <jeremymauzy@gmail.com>
2016-05-23 12:20:29 -0500

    Fix date formatting on submission result

--- client/js/components/DisplayLink.js.jsx
+++ client/js/components/DisplayLink.js.jsx
@@ -1,17 +1,25 @@
 import React from 'react';
+import moment from 'moment';
 
-module.exports = React.createClass ({
+var DisplayLink = React.createClass ({
 
   render: function() {
     var link = this.props.link
+    var formattedExpiration = moment(link.expiration*1000).format('MMMM do YYYY, hh:mm a')
     return (
       <div>
         <p>Your SecLink URL is localhost:3000/{link.url_string}</p>
         <p>This link redirects to {link.target_url}</p>
-        <p>It will expire on {link.expiration}</p>
+        <p>It will expire {formattedExpiration} (your local time)</p>
         <p>Or after {link.max_redirects} redirects</p>
-        <button className="btn btn-warning" onClick={this.props.resetApp}>Reset</button>
+        <button 
+          className="btn btn-warning" 
+          onClick={this.props.resetApp}>
+          Reset
+        </button>
       </div>
     );
   } 
 });
+
+module.exports = DisplayLink;
